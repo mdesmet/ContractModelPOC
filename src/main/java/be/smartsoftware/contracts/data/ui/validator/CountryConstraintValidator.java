96e933492ea579041f9dd069f0a47404b86a279f
Michiel De Smet <mdesmet@gmail.com>
2017-06-25 16:34:19 +0200

    Refactored to web application, added validation of reference values

---
+++ src/main/java/be/smartsoftware/contracts/data/ui/validator/CountryConstraintValidator.java
@@ -0,0 +1,44 @@
+package be.smartsoftware.contracts.data.ui.validator;
+
+import be.smartsoftware.contracts.data.reference.repo.ReferenceCountryRepository;
+import be.smartsoftware.contracts.data.ui.UICountry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.List;
+
+@Component
+public class CountryConstraintValidator implements ConstraintValidator<ValidCountry, List<UICountry>> {
+    private ReferenceCountryRepository referenceCountryRepository;
+
+    @Autowired
+    public CountryConstraintValidator(ReferenceCountryRepository referenceCountryRepository) {
+        this.referenceCountryRepository = referenceCountryRepository;
+    }
+
+    @Override
+    public void initialize(ValidCountry validCountry) {
+
+    }
+
+    @Override
+    public boolean isValid(List<UICountry> countries, ConstraintValidatorContext constraintValidatorContext) {
+        try {
+            countries.forEach(country -> {
+                if(referenceCountryRepository.findAll()
+                        .stream()
+                        .noneMatch(referenceCountry -> {
+                            if(country == null || referenceCountry == null)
+                                return false;
+                            return country.getCode().equals(referenceCountry.getCode());
+                        }))
+                    throw new RuntimeException();
+            });
+        } catch (RuntimeException e) {
+            return false;
+        }
+        return true;
+    }
+}
